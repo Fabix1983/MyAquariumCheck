43eb8b32b23caf93ae3a75557d62c4e58c6bd91b
Fabio Gianlorenzo <gianlorenzof@libero.it>
2024-05-26 11:14:49 +0200

    work in progress

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -11,15 +11,15 @@ public partial class ListPage : ContentPage
 
         var items = new List<FishList>
         {
-            new FishList { Nome ="Oranda", Descrizione="jfskjdfkjsdkjf", imgname="oranda.jpg"},
-            new FishList { Nome ="Oranda1", Descrizione="sdfsdfsd", imgname="ramirezi.jpg"},
-            new FishList { Nome ="Oranda2", Descrizione="45wererewr", imgname="betta.jpg"},
-            new FishList { Nome ="Oranda3", Descrizione="sdfdsfsdfsdfsd", imgname="platy.jpg"},
-            new FishList { Nome ="Oranda4", Descrizione="4354543534", imgname="discus.jpg"},
-            new FishList { Nome ="Oranda5", Descrizione="43refsdfsfs", imgname="corydoras.jpg"},
-            new FishList { Nome ="Oranda6", Descrizione="tryrtdf", imgname="scalare.jpg"},
-            new FishList { Nome ="Oranda7", Descrizione="jyujghjghj", imgname="guppy.jpg"},
-            new FishList { Nome ="Oranda8", Descrizione="sdfsdrhghgf", imgname="neon.jpg"},
+            new FishList { Nome ="Oranda", Descrizione="E' una varietà di pesce rosso e uno dei pochi pesci da acquario che si adatta bene ad un range di temperature dell'acqua comprese tra i 18° ed i 28° gradi, ideale tra i 22° e 26°. Presenta varie colorazioni come arancione, rosso, bianco e rosso, rosso e nero, nero, blu. Presente in diversi tipi di varietà, tra i più comuni troviamo Rosso, Shubunkin, Calico, Fantail, Ryukin, Testa di Leone, Black Moor e Ranchu. Puo raggiungere dimensioni da adulto comprese tra i 12 e 15 cm di lunghezza pertanto richiede acquari di alto litraggio, almeno 80/100 litri. Pesce longevo e resistente in condizioni ideali può vivere anche 20 anni.", imgname="oranda.jpg"},
+            new FishList { Nome ="Ramirezi", Descrizione="Appartiene alla famiglia dei ciclidi nani detti Ramirez. Pesce esigente e che richiede cura. Presenta colorazioni vivaci e puo raggiungere una dimensione di lunghezza di 7/9 cm.", imgname="ramirezi.jpg"},
+            new FishList { Nome ="Betta", Descrizione="Betta Splendens o anche detto Pesce combattente e un pesce elegante di dimensioni ridotte, puo raggiungere i 6/8 cm per i maschi e 5 cm per le femmine. In gradi di respirare ossigeno direttamente dall'aria tramite un organo detto labirinto. Pesce che non richiede alti litraggi e che non ama molto la compagnia. Le principali varietà sono Plakat, Veiltail,Halfmoon e la Crowntail. Predilige temperature tra i 25° e 28° gradi. Necessità di condizioni specifiche in particolare al PH affinche si possa mantenere in buona salute.", imgname="betta.jpg"},
+            new FishList { Nome ="Platy", Descrizione="sdfdsfsdfsdfsd", imgname="platy.jpg"},
+            new FishList { Nome ="Discus", Descrizione="4354543534", imgname="discus.jpg"},
+            new FishList { Nome ="Corydoras", Descrizione="43refsdfsfs", imgname="corydoras.jpg"},
+            new FishList { Nome ="Scalare", Descrizione="tryrtdf", imgname="scalare.jpg"},
+            new FishList { Nome ="Guppy", Descrizione="jyujghjghj", imgname="guppy.jpg"},
+            new FishList { Nome ="Neon", Descrizione="sdfsdrhghgf", imgname="neon.jpg"},
         };
 
         ListView.ItemsSource= items;  
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MyAquariumCheck
-Mobile application created in .NET 7.0 MAUI to memorize the controls of the aquarium values ♒
+Mobile APP realizzata in ASP.NET 8.0 MAUI, linguaggio C#, SQLite database interno, pattern MVVM, interfaccia XAML e LINQ per le operazioni sul db locale.
+
+Permette di memorizzare i controlli dei principali valori di un acquario di acqua dolce ossia la Temperatura, il PH, il KH, il GH, i Nitriti No2 ed i Nitrati No3.
+
+Presenta, oltre alla sezione di inserimento, una sezione di riepilogo con cancellazione tramite swipe, grafici di andamento ed una scheda dei principali pesci di acqua dolce utilizzati in acquariofilia.
+
+Licenza di rilascio: MIT
+
+Gianlorenzo Fabio
