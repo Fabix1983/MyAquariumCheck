060c4f4da334e00f11dc51ef45b8a03a1068893d
Fabio Gianlorenzo <gianlorenzof@libero.it>
2024-06-08 09:19:58 +0200

    agg

--- obj/Debug/net8.0-android/MyAquariumCheck.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/MyAquariumCheck.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAquariumCheck")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ead51acb4611a34c338fb9ad35eb3609e4a2ad8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAquariumCheck")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAquariumCheck")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
